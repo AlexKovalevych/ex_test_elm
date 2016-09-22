dffe968130d2bcf9d9bd21448882b2210495828c
Alex Kovalevych <alexkovalevych@gmail.com>
2016-09-22 09:20:30 +0300

    adding support of multiple datasets for the dashboard splines

--- web/static/elm/Native/Chart.js
+++ web/static/elm/Native/Chart.js
@@ -6,19 +6,22 @@ window.onresize = function() {
 
 var charts = {};
 
-var getDatasets = function(points, color) {
-    return [{
-        label: '',
-        fill: true,
-        data: points,
-        backgroundColor: 'rgba(' + color._0 + ',' + color._1 + ',' + color._2 + ', 0.2)',
-        borderColor: 'rgba(' + color._0 + ',' + color._1 + ',' + color._2 + ', 1)',
-        borderWidth: 1,
-        pointRadius: 1,
-        pointBorderWidth: 0,
-        pointHoverBorderWidth: 3,
-        pointHitRadius: 10
-    }];
+var getDatasets = function(datasets, colors) {
+    var data = [];
+    for (var i = 0; i < datasets.length; i++) {
+        data.push({
+            label: '',
+            fill: true,
+            data: datasets[i],
+            backgroundColor: 'rgba(' + colors[i]._0 + ',' + colors[i]._1 + ',' + colors[i]._2 + ', 0.2)',
+            borderColor: 'rgba(' + colors[i]._0 + ',' + colors[i]._1 + ',' + colors[i]._2 + ', 1)',
+            borderWidth: 1,
+            pointRadius: 1,
+            pointBorderWidth: 0,
+            pointHoverBorderWidth: 3,
+            pointHitRadius: 10
+        });
+    }
 };
 
 var getLabels = function(points) {
@@ -30,7 +33,7 @@ var getLabels = function(points) {
 };
 
 var _user$project$Native_Chart = function() {
-    function renderAreaChart(canvasId, color, data)
+    function renderAreaChart(canvasId, colors, data)
     {
         var ctx = document.getElementById(canvasId);
         if (ctx === null) {
@@ -40,11 +43,11 @@ var _user$project$Native_Chart = function() {
             return;
         }
 
-        var points = JSON.parse(data);
+        var datasets = JSON.parse(data);
         if (ctx.classList.contains('rendered')) {
             charts[canvasId].data = data;
-            charts[canvasId].chart.data.labels = getLabels(points);
-            charts[canvasId].chart.data.datasets = getDatasets(points, color);
+            charts[canvasId].chart.data.labels = getLabels(datasets._0);
+            charts[canvasId].chart.data.datasets = getDatasets(datasets, colors);
             charts[canvasId].chart.update();
         } else {
             ctx.setAttribute('class', 'rendered');
@@ -52,8 +55,8 @@ var _user$project$Native_Chart = function() {
             charts[canvasId]['chart'] = new Chart(ctx, {
                 type: 'line',
                 data: {
-                    labels: getLabels(points),
-                    datasets: getDatasets(points, color)
+                    labels: getLabels(datasets[0]),
+                    datasets: getDatasets(datasets, colors)
                 },
                 options: {
                     insertIframe: false,
@@ -113,7 +116,7 @@ var _user$project$Native_Chart = function() {
         }
     }
 
-    function renderBarChart(canvasId, color, data)
+    function renderBarChart(canvasId, colors, data)
     {
         var ctx = document.getElementById(canvasId);
         if (ctx === null) {
@@ -123,11 +126,11 @@ var _user$project$Native_Chart = function() {
             return;
         }
 
-        var points = JSON.parse(data);
+        var datasets = JSON.parse(data);
         if (ctx.classList.contains('rendered')) {
             charts[canvasId].data = data;
-            charts[canvasId].chart.data.labels = getLabels(points);
-            charts[canvasId].chart.data.datasets = getDatasets(points, color);
+            charts[canvasId].chart.data.labels = getLabels(datasets);
+            charts[canvasId].chart.data.datasets = getDatasets(datasets, colors);
             charts[canvasId].chart.update();
         } else {
             ctx.setAttribute('class', 'rendered');
@@ -135,8 +138,8 @@ var _user$project$Native_Chart = function() {
             charts[canvasId]['chart'] = new Chart(ctx, {
                 type: 'bar',
                 data: {
-                    labels: getLabels(points),
-                    datasets: getDatasets(points, color)
+                    labels: getLabels(datasets),
+                    datasets: getDatasets(datasets, colors)
                 },
                 options: {
                     insertIframe: false,
